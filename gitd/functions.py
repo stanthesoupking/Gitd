ec9e3ad7f5dc036d1bfcb45e430bb5d96b0b0341
Stanley Fuller <stanthesoupking@gmail.com>
2018-09-20 16:27:40 +1000

    Pull now deletes files that shouldn't exist

--- gitd/functions.py
+++ gitd/functions.py
@@ -2,6 +2,7 @@ import os
 from io import FileIO
 from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 from googleapiclient.errors import HttpError
+from shutil import rmtree
 
 # Useful functions
 
@@ -124,8 +125,6 @@ def push_from_folder(service, container, folder, recursive = True):
 
             service.files().create(body = body, media_body = media).execute()
 
-        
-    
     for ffolder in ffolders:
         updated = False
         for efolder in efolders:
@@ -147,15 +146,51 @@ def push_from_folder(service, container, folder, recursive = True):
             push_from_folder(service, container+ffolder+"/", result['id'])
 
 
-def pull_from_folder(service, container, folder, recursive = True):
+def pull_from_folder(service, container, folder, recursive = True, force = False):
     """Pulls files from the given folder ID and stores them in the container path.
     This function is set to recursive by default, so it will download files
-    inside sub-directories
+    inside sub-directories.
+    If force is set to True then the program will automatically assume that the
+    the user responds 'Yes' to any prompt.
     """
     container = to_path(container)
     q=f"'{folder}' in parents and trashed = false"
     files = service.files().list(q=q).execute()['files']
+    
+    # Check to see if any files need deleting
+    ffiles = [ x['name'] for x in files ]
+    to_delete = [ x for x in os.listdir(container) if x not in ffiles ]
 
+    # If files need to be deleted, ask user if they wish to proceed
+    if not force and (len(to_delete) > 0):
+        print("The following files/folders are present and need to be deleted in order to pull.")
+        for file in to_delete:
+            if(os.path.isdir(os.path.join(container, file))):
+                print(f" {file}/...")
+            else:
+                print(f" {file}")
+
+        ans = None
+        while(ans == None):
+            ans = input("Do you still wish to proceed (y/n)? ")
+            if(ans == 'y'):
+                ans = True
+            elif(ans == 'n'):
+                return
+            else:
+                print('Invalid input')
+                ans = None
+    
+    # Delete files that didn't exist on the Google Drive
+    for file in to_delete:
+        if(os.path.isdir(os.path.join(container, file))):
+            # Delete the directory
+            rmtree(os.path.join(container, file))
+        else:
+            # Delete the file
+            os.remove(os.path.join(container, file))
+
+    # Download files from Drive
     for file in files:
             if(file['mimeType'] == 'application/vnd.google-apps.folder'):
                 # File is of type folder, create folder an download it's files
